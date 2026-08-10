87f6629fe1c881cd8e82b6540bbe85b497189bb7
koenamk <koena.mukherjee@outlook.com>
2026-08-10 19:10:00 -0400

    Initial Commit

---
+++ QuoteWidgetHost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteWidgetHost
+//
+//  Created by Koena Mukherjee on 2026-08-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteWidgetHost/QuoteWidgetHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteWidgetHostApp.swift
+//  QuoteWidgetHost
+//
+//  Created by Koena Mukherjee on 2026-08-10.
+//
+
+import SwiftUI
+
+@main
+struct QuoteWidgetHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
